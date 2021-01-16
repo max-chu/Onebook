6a18e7c34afd3cad771b24ec6b874d664f53a46e
zhu-tom <zhutom01@gmail.com>
2021-01-16 04:54:11 -0500

    Complete db models

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
-
-
 const express = require("express");
+const cors = require('cors');
+const morgan = require('morgan');
 const bodyParser = require("body-parser");
+const sequelize = require("./sequelize");
+const { models } = sequelize;
 
 const app = express();
 
-app.use(bodyParser.json()).use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/ping", (req, res) => res.send("pong!"));
+
+app.get("/test", (req, res) => {
+  console.log(models);
+  models.user.create({email: "test@test.com"}).then(obj => {
+    res.send(obj.toJSON());
+  });
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Running on port " + PORT));
+
+sequelize.sync({force: true}).then(() => {
+  app.listen(PORT, () => console.log("Running on port " + PORT));
+});
