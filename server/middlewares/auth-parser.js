5f7736999c7ed2c2e0b4245135e2b3c69852f3b2
zhu-tom <zhutom01@gmail.com>
2021-01-16 13:54:32 -0500

    Connect to db cloud

--- server/middlewares/auth-parser.js
+++ server/middlewares/auth-parser.js
@@ -1,8 +1,30 @@
+require('dotenv').config({ path: '.env' });
+
+const { google } = require('googleapis');
+const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
+
+
 module.exports = (req, res, next) => {
   let auth = req.headers.authorization;
   if (!auth) {
-    res.status(401).send({message: "Missing authorization header"});
+    res.status(400).send({message: "Missing authorization header"});
   } else {
-    
+    auth = auth.split(" ");
+    if (auth.length !== 2) {
+      res.status(400).send({message: "Authorization header should be formatted 'Bearer token'"});
+    } else {
+      oAuth2Client.setCredentials(auth[1]);
+
+      google.people({version: 'v1', auth: oAuth2Client}).people.get({
+        resourceName: "people/me",
+        personFields: "emailAddresses"
+      }, (err, data) => {
+        if (err) res.status(500).send({message: 'Failed to verify authorization and email'});
+        else {
+          console.log(res.data);
+          next();
+        }
+      });
+    }
   }
 }
