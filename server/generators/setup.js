344b99a263663bb0810d104454c26e2aef353195
zhu-tom <zhutom01@gmail.com>
2021-01-16 02:06:20 -0500

    Set up cockroach and sequelize/pg

---
+++ server/generators/setup.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize-cockroachdb");
+const fs = require("fs");
+
+const sequelize = new Sequelize('onebook', 'maxroach', '', {
+  dialect: 'postgres',
+  port: 26257,
+  logging: false,
+  dialectOptions: {
+      ssl: {
+          ca: fs.readFileSync('certs/ca.crt')
+              .toString(),
+          key: fs.readFileSync('certs/client.maxroach.key')
+              .toString(),
+          cert: fs.readFileSync('certs/client.maxroach.crt')
+              .toString()
+      }
+  },
+  
+});
