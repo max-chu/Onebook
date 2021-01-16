f4da02b0ced1f084521a6e3f4a7a5c9ebfdd44ee
zhu-tom <zhutom01@gmail.com>
2021-01-16 13:54:32 -0500

    Connect to db cloud

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require("body-parser");
 const sequelize = require("./sequelize");
+const authParser = require("./middlewares/auth-parser");
+const { google } = require('googleapis');
 const { models } = sequelize;
 
 const app = express();
@@ -14,17 +16,16 @@ app.use(morgan("dev"));
 
 app.get("/ping", (req, res) => res.send("pong!"));
 
-app.get("/test", (req, res) => {
-  console.log(models);
-  models.user.create({email: "test@test.com"}).then(obj => {
-    res.send(obj.toJSON());
-  });
+app.get("/test", authParser, (req, res) => {
+  
 });
 
-app.post("/users")
+app.post("/users", authParser, (req, res) => {
+
+});
 
 const PORT = process.env.PORT || 5000;
 
-sequelize.sync({force: true}).then(() => {
+sequelize.sync(/* {force: true} */).then(() => {
   app.listen(PORT, () => console.log("Running on port " + PORT));
 });
--- server/sequelize/index.js
+++ server/sequelize/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config({path: '.env'});
+
 const Sequelize = require("sequelize-cockroachdb");
 const fs = require("fs");
 
@@ -11,18 +13,30 @@ const Friendship = require('./models/friendship');
 const { applyAssoc } = require("./assoc");
 const models = [User, Address, Link, PhoneNum, Relation, Tag, Friendship];
 
-const sequelize = new Sequelize('onebook', 'maxroach', '', {
+// const sequelize = new Sequelize('onebook', 'maxroach', '', {
+//   dialect: 'postgres',
+//   port: 26257,
+//   logging: console.log,
+//   dialectOptions: {
+//       ssl: {
+//           ca: fs.readFileSync('certs/ca.crt')
+//               .toString(),
+//           key: fs.readFileSync('certs/client.maxroach.key')
+//               .toString(),
+//           cert: fs.readFileSync('certs/client.maxroach.crt')
+//               .toString()
+//       }
+//   },
+// });
+const sequelize = new Sequelize('sugary-hyena-261.defaultdb', 'amruta', process.env.DB_PASS, {
   dialect: 'postgres',
+  host: "free-tier.gcp-us-central1.cockroachlabs.cloud",
   port: 26257,
   logging: console.log,
   dialectOptions: {
       ssl: {
-          ca: fs.readFileSync('certs/ca.crt')
+          ca: fs.readFileSync('certs/cc-ca.crt')
               .toString(),
-          key: fs.readFileSync('certs/client.maxroach.key')
-              .toString(),
-          cert: fs.readFileSync('certs/client.maxroach.crt')
-              .toString()
       }
   },
 });
